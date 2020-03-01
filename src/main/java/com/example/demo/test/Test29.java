a884ca79917cf57530dc54bc4203102435085b14
ruoan <549918019@qq.com>
2020-03-01 21:50:57 +0800

    强化stream

--- src/main/java/com/example/demo/test/Test29.java
+++ src/main/java/com/example/demo/test/Test29.java
@@ -8,8 +8,11 @@ import lombok.extern.slf4j.Slf4j;
 import java.io.IOException;
 import java.lang.reflect.Array;
 import java.util.*;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.function.ToIntFunction;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * stream
@@ -98,8 +101,132 @@ public class Test29 {
         users.forEach(Test29::getSome);
 
 
+        Integer[] nums = new Integer[10];
+        Stream<Integer> stream = Arrays.stream(nums);
+
+
+
+        //无限流 生成
+        Stream<Double> limit = Stream.generate(Math::random).limit(10);
+        limit.forEach(System.out::println);
+        //无限流 迭代
+        Stream<Integer> limit1 = Stream.iterate(1, (x) -> x + 2).limit(5);
+        limit1.forEach(System.out::println);
+
+
+        //HashMap<Object, Object> map = new HashMap<>();
+        List<Integer> list = new LinkedList<>();
+
+        list.add(4);
+
+        log.debug("当前list大小{}",list.size());
+
+
+        //distinct()去除重复元素
+        list.stream().distinct().collect(Collectors.toList()).forEach(System.out::println);
+
+
+        list.add(35);
+        list.add(16);
+        list.add(87);
+
+
+        // filter(),  过滤
+        List<Integer> collect1 = list.stream().filter(new Predicate<Integer>() {
+            @Override
+            public boolean test(Integer integer) {
+                return integer >= 10;
+            }
+
+        }).collect(Collectors.toList());
+
+        collect1.forEach(System.out::println);
+
+
+
+
+
+        //该函数会被应用到每个元素上，并将其映射成一个新的元素
+        list.stream().map(new Function<Integer, Object>() {
+            @Override
+            public Object apply(Integer integer) {
+                return String.valueOf(integer);
+            }
+        });
+
+        list.stream().flatMap(new Function<Integer, Stream<?>>() {
+            @Override
+            public Stream<?> apply(Integer integer) {
+                return Stream.of(integer);
+            }
+        });
+        System.out.println("xxxxxxxxxxxx");
+        list.stream().sorted().forEach(System.out::println);
+        System.out.println("xxxxxxxxxxxx");
+        // 从大到小排
+        list.stream().sorted(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer t1, Integer t2) {
+                if (t1 < t2)
+                    return 1;
+                else
+                    return -1;
+            }
+        }).forEach(System.out::println);
+
+
+
+        //终止操作
+
+        // allMatch
+        boolean b = list.stream().allMatch(new Predicate<Integer>() {
+            @Override
+            public boolean test(Integer integer) {
+                return true;
+            }
+        });
+        log.debug("是否全部符合条件{}",b);
+
+        list.add(4);
+
+        System.out.println("**********************");
+        list.forEach(System.out::println);
+        Optional<Integer> any = list.stream().findAny();
+        log.debug("从中任意返回一个值{}", any.get());
+
+
+
+        //max,min,count
+        Optional<Integer> max = list.stream().max(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer integer, Integer t1) {
+                if (integer > t1) {
+                    return 1;
+                }
+                return -1;
+            }
+        });
+        if (max.isPresent()) {
+            Integer integer = max.get();
+            System.out.println(integer);
+        }
+
+
+        long count = list.stream().count();
+        log.debug("当前流的总元素大小为:{}",count);
+
+
+
+        //reduce 归约
+
+
 
     }
+
+
+
+
+
 
 
     static void getSome(Object o){
@@ -107,6 +234,14 @@ public class Test29 {
     }
 
 
+}
+
+
+
+enum Status{
+    SLEPP,
+    EAT,
+    WORL
 }
 
 
---
+++ src/main/java/com/example/demo/test/Test32.java
@@ -0,0 +1,42 @@
+package com.example.demo.test;
+
+
+import cn.hutool.core.net.NetUtil;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.sql.SQLOutput;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ */
+@Slf4j
+public class Test32 {
+    public static int a=6;
+    public static int b=11;
+
+    public static void main(String[] args) throws IOException {
+
+
+        //Map<String, String> map = new HashMap<>();
+        //map.put("a", "a1");
+        //map.put("b", "b1");
+        //map.put("c", "c1");
+        //System.out.println(map.values());  //获取map所有的值
+
+        boolean usableLocalPort = NetUtil.isUsableLocalPort(6379);
+        System.out.println(usableLocalPort);
+    }
+
+
+    static void getSome(Object o){
+        System.out.println("哈哈");
+    }
+
+
+}
+
+
+
