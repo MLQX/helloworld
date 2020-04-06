f8bff13a71411f85e4b5dad09cb21abe1b1c93cd
ruoan <549918019@qq.com>
2020-04-06 10:38:40 +0800

    gc测试

--- src/main/java/com/example/demo/test/Test42.java
+++ src/main/java/com/example/demo/test/Test42.java
@@ -2,21 +2,41 @@ package com.example.demo.test;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * 异常
+ *   测试VM 老年代、新生代  MINOR GC & FULL GC
  */
 
 @Slf4j
 public class Test42 {
 
 
+    private byte[] b = new byte[1024 * 100];
+
     public static void main(String[] args) {
 
 
-
-
+        List<Test42> list = new ArrayList<>();
+        while (true) {
+            list.add(new Test42());
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
 
     }
 
 
+
+
+
+
 }
+
+
+
+
