7300c4628282dbbabedbd6e861ac9c4e6b8287ba
ruoan <549918019@qq.com>
2020-04-16 23:22:42 +0800

    增加新的测试文件

---
+++ src/main/java/com/example/demo/test/Test52.java
@@ -0,0 +1,39 @@
+package com.example.demo.test;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author ruoan
+ * @version 1.0
+ * @date 2020/4/8 19:43
+ */
+
+
+public class Test52 {
+    @Test
+    public void test1(){
+        List<String> li = new ArrayList<>();
+        //run(li); // List<Object>接口不是List<String>接口的父类
+        List<Object> li2 = new ArrayList<>();
+        run(li2);
+        run2(li);
+
+    }
+
+    public void run(List<Object> arg) {
+        for (int i = 0; i < arg.size(); i++) {
+            System.out.println(arg.get(i));
+        }
+    }
+    public void run2(List<?> arg) {
+        for (int i = 0; i < arg.size(); i++) {
+            System.out.println(arg.get(i));
+        }
+    }
+
+
+}
