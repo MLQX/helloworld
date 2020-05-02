31cf2b09e26496233f26357abc6d8523e811ca9a
ruoan <549918019@qq.com>
2020-05-02 22:27:14 +0800

    AES对称加密

---
+++ src/main/java/com/example/demo/test/Test57.java
@@ -0,0 +1,47 @@
+package com.example.demo.test;
+
+import cn.hutool.captcha.CaptchaUtil;
+import cn.hutool.captcha.LineCaptcha;
+import cn.hutool.core.lang.Console;
+import cn.hutool.crypto.SecureUtil;
+import cn.hutool.crypto.symmetric.SymmetricAlgorithm;
+import cn.hutool.crypto.symmetric.SymmetricCrypto;
+import io.netty.util.CharsetUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+/**
+ * @author ruoan
+ * @version 1.0
+ * @date 2020/4/8 19:43
+ *
+ *
+ * 加密工具
+ */
+
+@Slf4j
+public class Test57 {
+    /**
+     * 对称加密 AES
+     */
+    @Test
+    public void test1() {
+        String content = "test中文";
+        //随机生成密钥
+        byte[] key = SecureUtil.generateKey(SymmetricAlgorithm.AES.getValue()).getEncoded();
+        //Console.log(encoded);
+        //构建
+        SymmetricCrypto aes = new SymmetricCrypto(SymmetricAlgorithm.AES, key);
+        //加密
+        byte[] encrypt = aes.encrypt(content);
+        //解密
+        byte[] decrypt = aes.decrypt(encrypt);
+        //加密为16进制表示
+        String encryptHex = aes.encryptHex(content);
+        //解密为字符串
+        String decryptStr = aes.decryptStr(encryptHex, CharsetUtil.UTF_8);
+        Console.log("解密后的字符串为："+decryptStr);
+
+    }
+
+}
