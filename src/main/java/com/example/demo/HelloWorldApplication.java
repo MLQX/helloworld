4235515cefbfa0c5a3563387b0d3e027b8b43858
ruoan <549918019@qq.com>
2020-05-10 20:46:48 +0800

    测试shutdownHook

--- src/main/java/com/example/demo/HelloWorldApplication.java
+++ src/main/java/com/example/demo/HelloWorldApplication.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 //import com.example.demo.mq.consumer.NestedConsumer;
 //import com.example.demo.mq.producer.NestedProducer;
+
+import cn.hutool.core.util.RuntimeUtil;
 import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,9 +22,25 @@ public class HelloWorldApplication {
         //CronUtil.start();
 
 
-
         //生产者发送默认消息
 //        producer.produce(null);
+
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+
+            @Override
+            public void run() {
+                System.out.println("哈哈");
+                System.out.println("do ShutdownHook.......... ");
+                //防止内存泄漏
+                String ret1 = RuntimeUtil.execForStr("taskkill /im ffmpeg.exe /f");
+                String ret2 = RuntimeUtil.execForStr("taskkill /im ffmpeg /f");
+                System.out.println("clear Process step1:" + ret1);
+                System.out.println("clear Process step2:" + ret2);
+            }
+
+        });
+
+        System.out.println("******************************Version:0027***********************************");
 
 
     }
